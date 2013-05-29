59cf49ef4b3a4676fdb50d0fa10ee62550d22e05
Martin Senger <tulsoft@cpan.org>
2013-05-29 07:32:56 +0000

    Import of TULSOFT/Proc-Async-0.2.0 from CPAN.
    gitpan-cpan-distribution: Proc-Async
    gitpan-cpan-version:      0.2.0
    gitpan-cpan-path:         TULSOFT/Proc-Async-0.2.0.tar.gz
    gitpan-cpan-author:       TULSOFT
    gitpan-cpan-maturity:     released

--- lib/Proc/Async/Config.pm
+++ lib/Proc/Async/Config.pm
@@ -13,7 +13,7 @@ package Proc::Async::Config;
 
 use Carp;
 
-our $VERSION = '0.1.3'; # VERSION
+our $VERSION = '0.2.0'; # VERSION
 
 #-----------------------------------------------------------------
 # Constructor. It reads a given configuration file (but does not
@@ -158,7 +158,7 @@ Proc::Async::Config - Configuration helper
 
 =head1 VERSION
 
-version 0.1.3
+version 0.2.0
 
 =head1 AUTHOR
 
